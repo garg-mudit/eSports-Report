edcada4d527b525da05320890ae1386a4bb48510
Mudit <itzmuditgarg@gmail.com>
2022-02-19 21:42:36 +0530

    Initial commit from Create Next App

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found 😭</h2>
+    </div> 
+  )
+}
---
+++ pages/reports.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Reports.module.css'
+
+export default function Reports() {
+  return (
+    <div className={styles.reports}>
+      <h2>Latest Reports</h2>
+    </div> 
+  )
+}
